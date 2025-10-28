6a9aca2dfdb3645e9eea4ca565809edab87b5e68
devHksingh <hksingh.dev@gmail.com>
2025-10-28 12:30:36 +0530

    setup express app

---
+++ backend/src/app.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { config } from "./config";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import globalErrorHandler from "./middleware/globalErrorHandler";
+
+const app = express();
+
+const allowedOrigins = [config.clientUrl, config.DashboardUrl];
+
+// cors policy
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
+// security middleware
+app.use(helmet());
+app.use(hpp());
+
+// configuration
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
+
+// routes
+
+
+
+// Global error handler
+app.use(globalErrorHandler);
+
+export default app;
