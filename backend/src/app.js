7dd4111b1fb5490034d862c37775c708d5964ef4
devHksingh <hksingh.dev@gmail.com>
2025-10-30 16:31:36 +0530

    add route

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import hpp from "hpp";
 import globalErrorHandler from "./middleware/globalErrorHandler.js";
 import { config } from "./config/index.js";
+import authRouter from "./routes/auth.routes.js";
 
 const app = express();
 
@@ -36,6 +37,7 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
 // routes
+app.use("api/v1/auth",authRouter)
 
 // Global error handler
 app.use(globalErrorHandler);
